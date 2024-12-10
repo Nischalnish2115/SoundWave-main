f291dc5d97f1400a2bb447b3e20be35710611811
Nischalnish2115 <nischalnish46@gmail.com>
2024-12-10 20:38:22 +0530

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; SoundWave by nischal and sachitha.</span>
+          </div>
+        </div>
+      </footer>
